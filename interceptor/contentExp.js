50e516a199cc7f2d42eef906ccffc3ee3f36e746
pchristensenB <38443139+pchristensenB@users.noreply.github.com>
2025-01-31 10:50:26 +0000

    Updates to testbed

---
+++ interceptor/contentExp.js
@@ -0,0 +1,34 @@
+let contentExplorerOptions = {
+  "container": "#content-explorer",
+  "token": "YOUR_ACCESS_TOKEN",
+  "sharedLink": "YOUR_SHARED_LINK",
+  "size": "large",
+  "logoUrl": "https://your-logo-url.com/logo.png",
+  "canDownload": true,
+  "canPreview": true,
+  "canDelete": false,
+  "canUpload": true,
+  "canShare": true,
+  "canRename": false,
+  "canSetShareAccess": false,
+  "defaultView": "grid",
+  "sortBy": "name",
+  "sortDirection": "ASC",
+  "contentPreviewProps": {
+    "showAnnotations": true,
+    "contentSidebarProps": {
+      "detailsSidebarProps": {
+        "hasProperties": true,
+        "hasNotices": true,
+        "hasAccessStats": true,
+        "hasVersions": true,
+        "hasClassification": true,
+        "hasActivityFeed": true
+      },
+      "hasActivityFeed": true,
+      "hasMetadata": true,
+      "hasSkills": true,
+      "hasVersions": true
+    }
+  }
+}
